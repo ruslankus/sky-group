516513bed3151b41d8f8dfac79f42320274ea379
Rytis <rytis.grincevicius@gmail.com>
2015-04-29 15:50:13 +0300

    git

--- protected/models/forms/FormStep_3.php
+++ protected/models/forms/FormStep_3.php
@@ -24,21 +24,64 @@ class FormStep_3 extends CFormModel
 	{
 		return array(
 			// username and password are required
-			array('married', 'marriedRules'),
-            array('same_address', 'addressRules'),
+			array('married, partner_id, p_fname, p_lname, bday, bmonth, byear', 'marriedRules'),
+            array('same_address, street, house, flat, city, country, post_code', 'addressRules'),
 		);
 	}
     public function marriedRules($attributes, $params)
     {
-        return array(
-            array('partner_id', 'required'),
-        );
+        if ($this->married == 'yes') {
+            if ( empty( $this->partner_id )) {
+                  $this->addError('partner_id', "Требуется вход");
+             }
+            if ( empty( $this->p_fname )) {
+                  $this->addError('p_fname', "Требуется вход");
+             }
+            if ( empty( $this->p_lname )) {
+                  $this->addError('p_lname', "Требуется вход");
+             }
+            if ( empty( $this->bday )) {
+                  $this->addError('bday', "Требуется вход");
+             }
+            if ( empty( $this->byear )) {
+                  $this->addError('byear', "Требуется вход");
+             }
+            if ( empty( $this->bmonth )) {
+                  $this->addError('bmonth', "Требуется вход");
+             }
+            if ( !is_numeric( $this->byear )) {
+                  $this->addError('byear', "Неправильный выбор");
+             }
+            if ( !is_numeric( $this->bmonth )) {
+                  $this->addError('bmonth', "Неправильный выбор");
+             }
+            if ( !is_numeric( $this->bday )) {
+                  $this->addError('bday', "Неправильный выбор");
+             }
+        }
     }
     public function addressRules($attributes, $params)
     {
-        return array(
-            array('partner_id', 'required'),
-        );
+        if ($this->same_address == 'no') {
+            if ( empty( $this->street )) {
+                  $this->addError('street', "Требуется вход");
+             }
+            if ( empty( $this->house )) {
+                  $this->addError('house', "Требуется вход");
+             }
+            if ( empty( $this->flat )) {
+                  $this->addError('flat', "Требуется вход");
+             }
+            if ( empty( $this->city )) {
+                  $this->addError('city', "Требуется вход");
+             }
+            if ( empty( $this->country )) {
+                  $this->addError('country', "Требуется вход");
+             }
+            if ( empty( $this->post_code )) {
+                  $this->addError('post_code', "Требуется вход");
+             }
+        }
     }
 }
 
