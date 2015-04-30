698d6278e6a7f8d734182243f3d93a6b878c2464
Rytis <rytis.grincevicius@gmail.com>
2015-04-30 10:22:43 +0300

    step4

--- protected/models/forms/FormStep_4.php
+++ protected/models/forms/FormStep_4.php
@@ -2,22 +2,25 @@
 class FormStep_4 extends CFormModel
 {
     public $has_children;
-    public $children;
     
     public function rules()
 	{
 		return array(
 			// username and password are required
 			array('has_children', 'hasChildren'),
-            array('children','type','type'=>'array','allowEmpty'=>false),
 		);
 	}
     public function hasChildren ($atr, $params)
     {
         if ($this->has_children == 'yes') {
-           // foreach ($this->children as $child) {
-                //$this->addError($atr, print_r($this->children));
-        //    }
+            $children = $_REQUEST['children'];
+            print_r(array_filter($children));
+            $i=-1;
+            foreach ($children as $child) {
+                $i++;
+                $this->addError("children[{$i}][name]", 'what is that? '.$atr);
+                $this->addError("children[{$i}][surname]", 'what is that? '.$atr);
+            }
         }
     }
 }
--- protected/views/registration/en/_step_4.php
+++ protected/views/registration/en/_step_4.php
@@ -8,66 +8,55 @@
         <label data-name="children" class="radio <?php echo ($sessData['has_children'] != 'yes')? 'active' : ''; ?>" for="children-no">No</label>
         <input id="children-yes" type="radio" name="has_children" value="yes" <?php echo ($sessData['has_children'] == 'yes')? 'checked="checked"' : ''; ?>>
         <input id="children-no" type="radio" name="has_children" value="no" <?php echo ($sessData['has_children'] != 'yes')? 'checked="checked"' : ''; ?>>
-
         <div style="clear: both;"></div>
 
         <section class="offset <?php echo ($sessData['has_children'] == 'yes')? '' : 'hidden-block'; ?> if-children">
-
             <div class="children-list">
-
+            <?php
+                if ($errors) {
+                    foreach ($sessData['children'] as $id=>$child) {
+                ?>
+                <fieldset class="reg-3" id="children_<?php echo $id; ?>">
+                    <label class="bold-label"><?php echo $id+1; ?>st child</label>
+                    <input class="<?php echo $errors["children[$id][name]"] ? 'error' : '' ?>" data-error="<?php echo ($errors["children[$id][name]"] ? $errors["children[$id][name]"]:''); ?>" placeholder="Name" type="text" name="children[<?php echo $id; ?>][name]" value="<?php echo $sessData['children'][$id]['name']; ?>">
+                    <input class="<?php echo $errors["children[$id][surname]"] ? 'error' : '' ?>" data-error="<?php echo ($errors["children[{$id}][surname]"] ? $errors["children[{$id}][surname]"]:''); ?>" placeholder="Surname" type="text" name="children[<?php echo $id; ?>}][surname]" value="<?php echo $sessData['children'][$id]['surname']; ?>">
+                    <label>Date of birth</label>
+                    <div style="clear: both;"></div>
+                    <select class="<?php echo $errors["children[$id][day]"] ? 'error' : '' ?>" name="children[0][day]" class="selector-1">
+                        <option value="">Day</option>
+                    </select>
+                    <select class="<?php echo $errors["children[$id][month]"] ? 'error' : '' ?>" name="children[0][month]" class="selector-2">
+                        <option value="">Month</option>
+                    </select>
+                    <select class="<?php echo $errors["children[$id][year]"] ? 'error' : '' ?>" name="children[0][year]" class="selector-3">
+                        <option value="">Year</option>
+                    </select>
+                </fieldset>
+                <div style="clear: both;"></div>
+                <?php
+                    }
+                } else {
+                    ?>
                 <fieldset class="reg-3" id="children_0">
                     <label class="bold-label">1st child</label>
-                    <input data-error="<?php echo $errors['has_children']; ?>" placeholder="Name" type="text" name="children[0][name]" value="">
+                    <input placeholder="Name" type="text" name="children[0][name]" value="">
                     <input placeholder="Surname" type="text" name="children[0][surname]" value="">
                     <label>Date of birth</label>
                     <div style="clear: both;"></div>
                     <select name="children[0][day]" class="selector-1">
-                        <option>Day</option>
+                        <option value="">Day</option>
                     </select>
                     <select name="children[0][month]" class="selector-2">
-                        <option>Month</option>
+                        <option value="">Month</option>
                     </select>
                     <select name="children[0][year]" class="selector-3">
-                        <option>Year</option>
+                        <option value="">Year</option>
                     </select>
                 </fieldset>
                 <div style="clear: both;"></div>
-
-                <fieldset class="reg-3" id="children_1">
-                    <label class="bold-label">2nd child</label>
-                    <input data-error="<?php echo $errors['children']; ?>" placeholder="Name" type="text" name="children[1][name]" value="">
-                    <input placeholder="Surname" type="text" name="children[1][surname]" value="">
-                    <label>Date of birth</label>
-                    <div style="clear: both;"></div>
-                    <select name="children[1][day]" class="selector-1">
-                        <option>Day</option>
-                    </select>
-                    <select name="children[1][month]" class="selector-2">
-                        <option>Month</option>
-                    </select>
-                    <select name="children[1][year]" class="selector-3">
-                        <option>Year</option>
-                    </select>
-                </fieldset>
-                <div style="clear: both;"></div>
-
-                <fieldset class="reg-3" id="children_2">
-                    <label class="bold-label">3rd child</label>
-                    <input data-error="<?php echo $errors['children']; ?>" placeholder="Name" type="text" name="children[2][name]" value="">
-                    <input placeholder="Surname" type="text" name="children[2][surname]" value="">
-                    <label>Date of birth</label>
-                    <div style="clear: both;"></div>
-                    <select name="children[2][day]" class="selector-1">
-                        <option>Day</option>
-                    </select>
-                    <select name="children[2][month]" class="selector-2">
-                        <option>Month</option>
-                    </select>
-                    <select name="children[2][year]" class="selector-3">
-                        <option>Year</option>
-                    </select>
-                </fieldset>
-                <div style="clear: both;"></div>
+                <?
+                }
+            ?>
 
             </div>
             <a class="add-child-info" href="#">Add information about one more child</a>
