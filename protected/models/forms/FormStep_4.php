20d9ceb9ffba4d718052d237707d20fdfe552c8d
Rytis <rytis.grincevicius@gmail.com>
2015-05-04 15:11:46 +0300

    step 4

--- protected/models/forms/FormStep_4.php
+++ protected/models/forms/FormStep_4.php
@@ -13,30 +13,35 @@ class FormStep_4 extends CFormModel
     public function hasChildren ($atr, $params)
     {
         if ($this->has_children == 'yes') {
-            $children = isset($_REQUEST['children']) ? $_REQUEST['children']:'';
-            $i=-1;
-            
-            foreach ($children as $child) {
-                $i++;
-                //first child is required, other only if atleast one input submited
-                if ( $i == 0 || ($child['name'] || $child['surname'] || $child['day'] || $child['month'] || $child['year']) ) {
-                    
-                    if (empty($child['name'])) {
-                        $this->addError("children_".$i."_name", Yii::t('yii.skygroup','Field cannot be blank.'));
-                    }
-                    if (empty($child['surname'])) {
-                        $this->addError("children_".$i."_surname", Yii::t('yii.skygroup','Field cannot be blank.'));
-                    }
-                    if (empty($child['day'])) {
-                        $this->addError("children_".$i."_day", Yii::t('yii.skygroup','Field cannot be blank.'));
-                    }
-                    if (empty($child['month'])) {
-                        $this->addError("children_".$i."_month", Yii::t('yii.skygroup','Field cannot be blank.'));
-                    }
-                    if (empty($child['year'])) {
-                        $this->addError("children_".$i."_year", Yii::t('yii.skygroup','Field cannot be blank.'));
+            if(isset($_REQUEST['children'])) {
+                $children = $_REQUEST['children'];
+                $i=-1;
+
+                foreach ($children as $child) {
+                    $i++;
+                    //first child is required, other only if atleast one input submited
+                    if ( $i == 0 || ($child['name'] || $child['surname'] || $child['day'] || $child['month'] || $child['year']) ) {
+
+                        if (empty($child['name'])) {
+                            $this->addError("children_".$i."_name", Yii::t('skygroup','Field cannot be blank.'));
+                        }
+                        if (empty($child['surname'])) {
+                            $this->addError("children_".$i."_surname", Yii::t('skygroup','Field cannot be blank.'));
+                        }
+                        if (empty($child['day'])) {
+                            $this->addError("children_".$i."_day", Yii::t('skygroup','Field cannot be blank.'));
+                        }
+                        if (empty($child['month'])) {
+                            $this->addError("children_".$i."_month", Yii::t('skygroup','Field cannot be blank.'));
+                        }
+                        if (empty($child['year'])) {
+                            $this->addError("children_".$i."_year", Yii::t('skygroup','Field cannot be blank.'));
+                        }
                     }
                 }
+            }
+            else {
+                $this->addError("children_0_name", Yii::t('skygroup','Field cannot be blank.'));
             }
         }
     }
